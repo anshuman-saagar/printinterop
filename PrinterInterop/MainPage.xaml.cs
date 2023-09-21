eccf03b91b60de58977507a1d28ba8b0db1c4084
binu <binu@MOBIUX-WINDOWS>
2023-09-21 13:16:12 +0530

    Add project files.

---
+++ PrinterInterop/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PrinterInterop
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        StorageFile imageFile = null;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (imageFile != null)
+                ApplicationData.Current.LocalSettings.Values["FileToPrint"] = imageFile.Path;
+
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            {
+                await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            }
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".png");
+            imageFile = await picker.PickSingleFileAsync();
+
+            if (imageFile != null)
+            {
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.SetSource(await imageFile.OpenAsync(FileAccessMode.Read));
+                img.Source = bitmap;
+            }
+        }
+    }
+}
---
+++ Win32Printer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Storage;
+using System.IO;
+using Windows.UI.Xaml.Shapes;
+
+namespace Win32Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoResetEvent resetEvent = new AutoResetEvent(false);
+            if (ApplicationData.Current.LocalSettings.Values.ContainsKey("FileToPrint"))
+            {
+                // Image img = Image.FromFile(ApplicationData.Current.LocalSettings.Values["FileToPrint"] as string);
+                FileStream fs = new FileStream(@"C:\test.pdf", FileMode.Open);
+                Image img = Image.FromStream(fs);
+                PrintDocument doc = new PrintDocument();
+                doc.PrintPage += new PrintPageEventHandler((sender, e)=>
+                {
+                    img = ResizeImage(img, e.Graphics.VisibleClipBounds.Size);
+                    e.Graphics.DrawImage(img, Point.Empty);
+                    e.HasMorePages = false;
+                });
+                doc.EndPrint += new PrintEventHandler((sender, e) =>
+                {
+                    resetEvent.Set();
+                });
+                doc.Print();
+            }
+            resetEvent.WaitOne();
+        }
+
+        public static Image ResizeImage(Image img, SizeF targetSize)
+        {
+            float scale = Math.Min(targetSize.Width / img.Width, targetSize.Height / img.Height);
+            Size newSize = new Size((int)(img.Width * scale) - 1, (int)(img.Height * scale) - 1);
+            return new Bitmap(img, newSize);
+        }
+    }
+}
